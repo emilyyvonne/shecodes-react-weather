ac94887f3724ea99c9102d58df59293469142ed1
emilyyvonne <148805367+emilyyvonne@users.noreply.github.com>
2024-06-09 16:33:15 -0400

    re-did project base

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="Harrisburg" />
+
+        <footer>
+          This project was coded by Emily Brisbin and is{" "}
+          <a
+            href="htthttps://github.com/emilyyvonne"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://zippy-kitsune-0df230.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
